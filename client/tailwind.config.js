48e0bf7e0fdf59be18a6e3273bd0f7c348ae4a8c
RaviSharma <20uec104@lnmiit.ac.in>
2024-10-01 20:53:58 +0530

    shadcn ui setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     darkMode: ["class"],
     content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
   theme: {
   	extend: {
   		borderRadius: {
